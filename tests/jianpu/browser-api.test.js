cc958170879a00288f4cdb9688be67a785dc7a5a
ZeppelinTsai <b131ab131a@gmail.com>
2026-08-01 23:53:31 +0800

    改良播放顯示同步動畫

--- tests/jianpu/browser-api.test.js
+++ tests/jianpu/browser-api.test.js
@@ -7,19 +7,19 @@ var tuneBook = require("../../src/api/abc_tunebook");
 var jianpu = require("../../src/jianpu");
 
 var abc = [
-	"X:1",
-	"T:Test Tune",
-	"M:4/4",
-	"L:1/4",
-	"K:C",
-	"C D E F | G2 A/B/ z | [CEG] ^F2 |]",
+  "X:1",
+  "T:Test Tune",
+  "M:4/4",
+  "L:1/4",
+  "K:C",
+  "C D E F | G2 A/B/ z | [CEG] ^F2 |]",
 ].join("\n");
 var text = [
-	"T:Octave and Duration Example",
-	"M:4/4",
-	"K:C",
-	"1, 2,, 3 4' | 5'' 6_ 7_ 1- | 2. 3__ 4__ 5- |",
-	"6.. 7__ 1- | 2--- | 0 0 0 0 |]",
+  "T:Octave and Duration Example",
+  "M:4/4",
+  "K:C",
+  "1, 2,, 3 4' | 5'' 6_ 7_ 1- | 2. 3__ 4__ 5- |",
+  "6.. 7__ 1- | 2--- | 0 0 0 0 |]",
 ].join("\n");
 
 var abcResult = jianpu.fromAbc(abc, { staffWidth: 800 }, tuneBook.parseOnly);
@@ -33,62 +33,72 @@ assert.strictEqual(abcResult.layout.lines.length, 1);
 assert.strictEqual(textResult.layout.lines.length, 1);
 
 var abcWithWarning = jianpu.fromAbc(
-	"X:1\nK:C\nC ? D |]",
-	{ staffWidth: 800 },
-	tuneBook.parseOnly
+  "X:1\nK:C\nC ? D |]",
+  { staffWidth: 800 },
+  tuneBook.parseOnly,
 );
 assert.ok(abcWithWarning.warnings.length > 0);
 
 var fingeringResult = jianpu.fromAbc(
-	"X:1\nK:C\n\"Am\"\"^1\"c' |]",
-	{ staffWidth: 300 },
-	tuneBook.parseOnly
+  'X:1\nK:C\n"Am""^1"c\' |]',
+  { staffWidth: 300 },
+  tuneBook.parseOnly,
 );
-var fingeringEvent =
-	fingeringResult.layout.lines[0].measures[0].events[0];
+var fingeringEvent = fingeringResult.layout.lines[0].measures[0].events[0];
 assert.strictEqual(fingeringEvent.annotations.fingerings[0].text, "1");
 assert.strictEqual(
-	fingeringEvent.notePositions[0].octaveDotPositions.length,
-	2
+  fingeringEvent.notePositions[0].octaveDotPositions.length,
+  2,
 );
 assert.ok(
-	fingeringEvent.annotations.fingerings[0].cy <
-		fingeringEvent.notePositions[0].octaveDotPositions[0].cy
+  fingeringEvent.annotations.fingerings[0].cy <
+    fingeringEvent.notePositions[0].octaveDotPositions[0].cy,
 );
 assert.ok(
-	fingeringEvent.annotations.chords[0].y <
-		fingeringEvent.annotations.fingerings[0].cy -
-			fingeringEvent.annotations.fingerings[0].r
+  fingeringEvent.annotations.chords[0].y <
+    fingeringEvent.annotations.fingerings[0].cy -
+      fingeringEvent.annotations.fingerings[0].r,
 );
 assert.ok(
-	fingeringEvent.notePositions[0].octaveDotPositions.reduce(
-			function(top, dot) { return Math.min(top, dot.cy - dot.r); },
-			Infinity
-		) -
-		(fingeringEvent.annotations.fingerings[0].cy +
-			fingeringEvent.annotations.fingerings[0].r) >=
-		jianpu.layoutJianpu.DEFAULT_OPTIONS.fingeringGap
+  fingeringEvent.notePositions[0].octaveDotPositions.reduce(function (
+    top,
+    dot,
+  ) {
+    return Math.min(top, dot.cy - dot.r);
+  }, Infinity) -
+    (fingeringEvent.annotations.fingerings[0].cy +
+      fingeringEvent.annotations.fingerings[0].r) >=
+    jianpu.layoutJianpu.DEFAULT_OPTIONS.fingeringGap,
 );
 assert.match(fingeringResult.svg, /class="jianpu-fingering-circle"/);
-assert.match(fingeringResult.svg, /class="jianpu-fingering-text"[^>]*>1<\/text>/);
+assert.match(
+  fingeringResult.svg,
+  /class="jianpu-fingering-text"[^>]*>1<\/text>/,
+);
 
 var playground = fs.readFileSync(
-	path.resolve(__dirname, "../../index.html"),
-	"utf8"
+  path.resolve(__dirname, "../../index.html"),
+  "utf8",
 );
 assert.match(playground, /列印／儲存 PDF/);
 assert.match(playground, /@media print/);
 assert.match(playground, /window\.print\(\)/);
 
-console.log(JSON.stringify({
-	abc: {
-		width: abcResult.layout.width,
-		lines: abcResult.layout.lines.length,
-		warnings: abcResult.warnings,
-	},
-	text: {
-		width: textResult.layout.width,
-		lines: textResult.layout.lines.length,
-		warnings: textResult.warnings,
-	},
-}, null, 2));
+console.log(
+  JSON.stringify(
+    {
+      abc: {
+        width: abcResult.layout.width,
+        lines: abcResult.layout.lines.length,
+        warnings: abcResult.warnings,
+      },
+      text: {
+        width: textResult.layout.width,
+        lines: textResult.layout.lines.length,
+        warnings: textResult.warnings,
+      },
+    },
+    null,
+    2,
+  ),
+);
